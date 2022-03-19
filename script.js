045a43b61441805fbed9097394a6cb35a7b5183d
Lumenatom <89205842+Lumenatom@users.noreply.github.com>
2022-03-19 14:09:08 +0300

    full

---
+++ script.js
@@ -0,0 +1,17 @@
+setInterval(() => {
+    const time = document.querySelector(".display");
+    let date = new Date();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+    if (hours < 10) {
+        hours = "0" + hours;
+    }
+    time.textContent = hours + ":" + minutes + ":" + seconds;
+})
